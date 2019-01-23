da1316a53219b1666367f2d9a2d5580efccf807f
irmalien <mikus.vanags@gmail.com>
2019-01-23 10:50:43 +0100

    added components

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
-// import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// function getButtonText(){
-//   return "Click on me!";
-// }
+import App from './components/App'
 
 // create a react component
-const App = () => {
-  return (
-  <div>
 
-  </div>
-); 
-}
+
 
 // Take the react component and show on screen
-
-ReactDOM.render( <
-  App / > ,
+ReactDOM.render( 
+  <App /> ,
   document.querySelector('#root')
 );
